1d337c1eb153c942dc003a7fbb531b8b1d8277e4
deb <debkbanerji@gmail.com>
2019-03-22 06:58:56 -0400

    Fix naming

--- dist-utility-files/server.js
+++ dist-utility-files/server.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 
-const frontendFolder = path.join(__dirname, 'office-hours-queue');
+const frontendFolder = path.join(__dirname, 'kepler-eas-project');
 // Point static path to frontend folder
 app.use(express.static(frontendFolder));
 console.log("Serving static from " + frontendFolder);
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to office-hours-queue!');
+    expect(page.getParagraphText()).toEqual('Welcome to kepler-eas-project!');
   });
 });
